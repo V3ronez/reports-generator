ff9e8994d89ba4f7e162aa8f2f286fa0a25b23bd
Henrique Veronez <v3ronez.dev@gmail.com>
2023-01-29 16:53:58 -0300

    pipe operator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,34 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.reduce(%{}, fn line, acc ->
+      [id, _food_name, price] = parse_line(line)
+      Map.put(acc, id, price)
+    end)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
   end
 end
+
+# metodo de ler arquivo usando case
+# {:ok, result} ->
+#   result
+
+# {:error, reason} ->
+#   reason
+# se não dar match ele cai no default value
+# _ -> "case default"
+
+# metodo elegante de ler o arquivo
+# defp handle_file_content({:ok, file_content}), do: file_content
+# defp handle_file_content({:error, _reason}), do: "Error to read file"
+
+# criando funcao de forma implicita.
+# & = chama uma funcao
+# &String.to_integer/1 chamando essa funcao e passando o unico elemento para ela como primeiro argumento explicitamente
