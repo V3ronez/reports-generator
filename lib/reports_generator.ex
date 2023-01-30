273c911f27815742f70db89278617c9a73b8083e
Henrique Veronez <v3ronez.dev@gmail.com>
2023-01-30 15:01:03 -0300

    refactoring code

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,12 +1,13 @@
 defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
   def build(filename) do
     filename
-    |> ReportsGenerator.Parser.parse_file()
-    |> Enum.reduce(report_acc(), fn [id, _food_name, price], acc ->
-      Map.put(acc, id, acc[id] + price)
-    end)
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, acc -> sum_line(line, acc) end)
   end
 
+  defp sum_line([id, _food_name, price], acc), do: Map.put(acc, id, acc[id] + price)
   defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
 end
 
